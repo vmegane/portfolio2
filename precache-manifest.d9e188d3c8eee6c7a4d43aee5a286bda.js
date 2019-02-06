e73e963416e5fc9f364024a4dc24621c5a9389e9
Paulina Opacka <paulina.opacka@gmail.com>
2019-02-06 21:49:05 +0100

    Updates

---
+++ precache-manifest.d9e188d3c8eee6c7a4d43aee5a286bda.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "06468580303d77d9afd2801a2ca80086",
+    "url": "/Portfolio/static/media/facebook.06468580.svg"
+  },
+  {
+    "revision": "fe0b864d3d2f41e802f9",
+    "url": "/Portfolio/static/css/main.f8802213.chunk.css"
+  },
+  {
+    "revision": "28b3ee442534c0f7ec65",
+    "url": "/Portfolio/static/js/1.28b3ee44.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "73e8a8ac6fa479ec19e5fe0cb7a81c01",
+    "url": "/Portfolio/static/media/linkedin.73e8a8ac.svg"
+  },
+  {
+    "revision": "fe0b864d3d2f41e802f9",
+    "url": "/Portfolio/static/js/main.fe0b864d.chunk.js"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/Portfolio/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "b050376405aaf7b4ca36ca34149f9b30",
+    "url": "/Portfolio/static/media/instagram.b0503764.svg"
+  },
+  {
+    "revision": "fccfc22cd2ebef1c6c07f2cc02b2ce44",
+    "url": "/Portfolio/static/media/me.fccfc22c.jpg"
+  },
+  {
+    "revision": "d4739f5ad132cc643a01a4e2697686ae",
+    "url": "/Portfolio/static/media/bg-image-iceland.d4739f5a.jpg"
+  },
+  {
+    "revision": "97a83c6c8254a3be07ea436ac3dc9dfe",
+    "url": "/Portfolio/index.html"
+  }
+];
