5fa5ea553b8e274317c3ae84d66f31f5a1c23171
Paulina Opacka <paulina.opacka@gmail.com>
2019-08-27 21:59:13 +0200

    Updates

---
+++ precache-manifest.1cacf785bb32e82f85c409a7c06c04f7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "f986319838f459296244d999d2591513",
+    "url": "/portfolio2/static/media/ember.f9863198.png"
+  },
+  {
+    "revision": "01b8e2836b0ed4b1ac21",
+    "url": "/portfolio2/static/css/main.905b341c.chunk.css"
+  },
+  {
+    "revision": "fa5be7429db0bdfd074a",
+    "url": "/portfolio2/static/js/1.fa5be742.chunk.js"
+  },
+  {
+    "revision": "099cc4b1c2dc80af7879",
+    "url": "/portfolio2/static/js/runtime~main.099cc4b1.js"
+  },
+  {
+    "revision": "c05063822c40b64747d467189db545ec",
+    "url": "/portfolio2/static/media/portfolio.c0506382.jpg"
+  },
+  {
+    "revision": "2393363c686c8abaa326e1cee5040c6f",
+    "url": "/portfolio2/static/media/memory.2393363c.jpg"
+  },
+  {
+    "revision": "de86067c28d74afc6fe2b4abe4ba271c",
+    "url": "/portfolio2/static/media/roomies.de86067c.png"
+  },
+  {
+    "revision": "01b8e2836b0ed4b1ac21",
+    "url": "/portfolio2/static/js/main.01b8e283.chunk.js"
+  },
+  {
+    "revision": "73e8a8ac6fa479ec19e5fe0cb7a81c01",
+    "url": "/portfolio2/static/media/linkedin.73e8a8ac.svg"
+  },
+  {
+    "revision": "67910a24ef067bf5dc2400ab841889c4",
+    "url": "/portfolio2/static/media/github.67910a24.svg"
+  },
+  {
+    "revision": "b050376405aaf7b4ca36ca34149f9b30",
+    "url": "/portfolio2/static/media/instagram.b0503764.svg"
+  },
+  {
+    "revision": "d4739f5ad132cc643a01a4e2697686ae",
+    "url": "/portfolio2/static/media/bg-image-iceland.d4739f5a.jpg"
+  },
+  {
+    "revision": "2ebedcc2a99a1366d319fc55fb609a44",
+    "url": "/portfolio2/static/media/me.2ebedcc2.jpg"
+  },
+  {
+    "revision": "fcf15d16b564a13c2252168868deb831",
+    "url": "/portfolio2/static/media/bg-image-iceland-mobile.fcf15d16.png"
+  },
+  {
+    "revision": "e7480251f6943f1e554396c7f07824d4",
+    "url": "/portfolio2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.f87f7b294aea0c560eb64ba854abd655.js"
+  "/portfolio2/precache-manifest.1cacf785bb32e82f85c409a7c06c04f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
